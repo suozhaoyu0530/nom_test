7978e3dbe996645909c7e6158f40e3a5c577035b
suozhaoyu <suozhaoyu0530@163.com>
2019-06-14 10:17:32 +0800

    在field_parse中增加只针对field的测试

--- src/nom1/field_parse.rs
+++ src/nom1/field_parse.rs
@@ -5,6 +5,7 @@ use crate::commons::{sql_alphanumeric, as_alias, number_alphanumeric};
 
 use super::field_expr::{Field, FieldExpr, FixedValue, ValueType, Function};
 use super::{SelectStatementChild, select_statement};
+use crate::nom1::SelectStatement;
 
 named!(pub field_list<CompleteByteSlice, Vec<FieldExpr>>,
     many0!(
@@ -136,3 +137,57 @@ named!(function_reference<CompleteByteSlice, Function>,
         })
     )
 );
+
+#[test]
+fn field_test() {
+    let field = "name n, t.age, 'from' as 'a', 1 as num, (select child from) child";
+    let field_slice = CompleteByteSlice(field.as_bytes());
+
+    let f1 = Field {
+        table: None,
+        name: String::from("name"),
+        alias: Some(String::from("n"))
+    };
+    let fe1 = FieldExpr::Normal(f1);
+
+    let f2 = Field {
+        table: Some(String::from("t")),
+        name: String::from("age"),
+        alias: None
+    };
+    let fe2 = FieldExpr::Normal(f2);
+
+    let f3 = FixedValue {
+        value: String::from("from"),
+        value_type: ValueType::STRING,
+        alias: Some(String::from("a"))
+    };
+    let fe3 = FieldExpr::FixedValue(f3);
+
+    let f4 = FixedValue {
+        value: String::from("1"),
+        value_type: ValueType::NUMBER,
+        alias: Some(String::from("num"))
+    };
+    let fe4 = FieldExpr::FixedValue(f4);
+
+    let fc1 = Field {
+        table: None,
+        name: String::from("child"),
+        alias: None
+    };
+    let fec1 = FieldExpr::Normal(fc1);
+    let fecv1 = vec![fec1];
+    let s1 = SelectStatement {
+        fields: fecv1
+    };
+    let sc1 = SelectStatementChild {
+        select_statement: s1,
+        alias: Some(String::from("child"))
+    };
+    let fe5 = FieldExpr::Statement(sc1);
+
+    let fev = vec![fe1, fe2, fe3, fe4, fe5];
+
+    assert_eq!(fev, field_list(field_slice).unwrap().1);
+}
